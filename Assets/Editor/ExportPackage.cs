2b7f848e3f67c9d67e1601d8a913fd814bb6f4ca
Takashi Sakai <sakai@mobcast.jp>
2017-09-28 18:54:56 +0900

    export package automatically

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+namespace Mobcast.Coffee.Symbol
+{
+	public static class ExportPackage
+	{
+		const string kPackageName = "SymbolCatalog.unitypackage";
+		static readonly string[] kAssetPathes = {
+			"Assets/Mobcast/Coffee/Editor/SymbolCatalog",
+		};
+
+		[MenuItem ("Export Package/" + kPackageName)]
+		[InitializeOnLoadMethod]
+		static void Export ()
+		{
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
+				return;
+			
+			AssetDatabase.ExportPackage (kAssetPathes, kPackageName, ExportPackageOptions.Recurse | ExportPackageOptions.Default);
+			UnityEngine.Debug.Log ("Export successfully : " + kPackageName);
+		}
+	}
+}
