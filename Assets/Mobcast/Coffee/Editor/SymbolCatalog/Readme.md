d64cdf706548b4981f9eab971420817a71f438b4
Takashi Sakai <sakai@mobcast.jp>
2017-09-29 14:56:11 +0900

    0.2.0: Apply to all targets

--- Assets/Mobcast/Coffee/Editor/SymbolCatalog/Readme.md
+++ Assets/Mobcast/Coffee/Editor/SymbolCatalog/Readme.md
@@ -5,7 +5,7 @@ SymbolCatalog
 
 A tool for edit script symbol define for Unity.
 
-![image](https://user-images.githubusercontent.com/12690315/30959357-d6234108-a47a-11e7-8c3e-a06f3bd59cb0.png)
+![image](https://user-images.githubusercontent.com/12690315/31002438-c3cd716c-a525-11e7-809c-ffe76f071252.png)
 
 ## Requirement
 
@@ -18,19 +18,26 @@ A tool for edit script symbol define for Unity.
 ## Usage
 
 1. Download [SymbolCatalog.unitypackage](https://github.com/mob-sakai/SymbolCatalog/raw/master/SymbolCatalog.unitypackage) and install to your project.
-2. From the menu, click `Coffee > SymbolCatalog`
+1. From the menu, click `Coffee > Symbol Catalog`
+1. Edit symbol catalog as following:
     * ![image](https://user-images.githubusercontent.com/12690315/30959671-da6291aa-a47b-11e7-8d4d-043b9496a030.png) : Add new symbol/header/separator to catalog
-    * ![image](https://user-images.githubusercontent.com/12690315/30959706-f17632c0-a47b-11e7-9c59-d06ded5b381c.png) : Remove selected symbol from catalog
+    * ![image](https://user-images.githubusercontent.com/12690315/30959706-f17632c0-a47b-11e7-9c59-d06ded5b381c.png) ![image](https://user-images.githubusercontent.com/12690315/30998347-c97625c2-a508-11e7-8f1a-f8526cbc61a5.png) : Remove symbol from catalog
     * ![image](https://user-images.githubusercontent.com/12690315/30959891-72ef4e90-a47c-11e7-88d6-ef8ab4b67eb0.png) : Toggle symbol
-    * ![image](https://user-images.githubusercontent.com/12690315/30959779-21e01516-a47c-11e7-9dbd-a4e532530a5e.png) : Reorder symbol
-    * Edit symbol name and comment
-3. Click ![image](https://user-images.githubusercontent.com/12690315/30959836-5034e2de-a47c-11e7-9b1e-947236b8e3ae.png) to apply script symbol define.
-4. Enjoy!
+    * ![image](https://user-images.githubusercontent.com/12690315/30959779-21e01516-a47c-11e7-9dbd-a4e532530a5e.png) : Drag and drop to reorder symbol
+    * Edit symbol name or description
+1. Click `All Targets` or `Current (xxx)` to apply scripting symbol define in project setting.
+1. Automatically start compiling.
+1. Enjoy!
 
 
 
 
 ## Release Notes
+
+### ver.0.2.0:
+
+* Feature: Delete button at each symbols.
+* Feature: Apply to all targets
 
 ### ver.0.1.0:
 
--- Assets/Mobcast/Coffee/Editor/SymbolCatalog/SymbolCatalog.cs
+++ Assets/Mobcast/Coffee/Editor/SymbolCatalog/SymbolCatalog.cs
@@ -1,49 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
-using UnityEditor.Callbacks;
 using UnityEditorInternal;
 using UnityEngine;
-using System.Reflection;
 
 namespace Mobcast.Coffee.Editor
 {
 	/// <summary>
 	/// [アセット]定義シンボルカタログクラス.
 	/// プロジェクトで利用できる定義シンボルをカタログ化し、設定/共有しやすい機能を提供します.
+	/// Symbol Catalog.
+	/// Catalog 'Scripting Define Symbols' available in the project, for setting and sharing.
 	/// </summary>
 	public class SymbolCatalog : ScriptableObject
 	{
-		/// <summary>定義シンボルリスト.</summary>
+		/// <summary>
+		/// 定義シンボルリスト.
+		/// Scripting Define Symbols.
+		/// </summary>
 		public List<Symbol> list = new List<Symbol>();
 
-		/// <summary>有効な定義シンボルリストを適用します.</summary>
-		public void Apply()
+		/// <summary>
+		/// 有効な定義シンボルリストを適用します.
+		/// Apply 'Scripting Define Symbols' to project settings.
+		/// </summary>
+		public void Apply(params BuildTargetGroup[] targetGroups)
 		{
-			//シンボルリストから、有効なシンボルのみ取得.
+			// 
+			targetGroups =  0 < targetGroups.Length
+				? targetGroups
+				: (BuildTargetGroup[])System.Enum.GetValues(typeof(BuildTargetGroup));
+			
+			// シンボルリストから、有効なシンボルのみ取得.
+			// Collect active symbols.
 			List<string> defines = list
-				.Where(x => x.style != SymbolStyle.Separator && !string.IsNullOrEmpty(x.name) && x.enabled)
+				.Where(x => x.style == SymbolStyle.Symbol && !string.IsNullOrEmpty(x.name) && x.enabled)
 				.Select(x => x.name)
 				.Distinct()
 				.ToList();
 		
-			//定義シンボルを文字列に変換し、全プラットフォームに適用.
+			// 有効なシンボルを文字列に変換します.
+			// Convert active symbols to string.
 			string defineSymbols = defines.Any() ? defines.Aggregate((a, b) => a + ";" + b) : string.Empty;
-			Apply(defineSymbols);
-		}
 
-		/// <summary>有効な定義シンボルリストを適用します.</summary>
-		public void Apply(string defineSymbols)
-		{
-			//全てのターゲットグループの定義シンボルに適用.
-			foreach (BuildTargetGroup group in System.Enum.GetValues(typeof(BuildTargetGroup)))
+			Debug.Log(defineSymbols);
+			// ターゲットグループの定義シンボルに適用.
+			// Apply to build target platforms.
+			foreach (BuildTargetGroup group in targetGroups)
 			{
+				// Group is Obsoleted.
+				if (typeof(BuildTargetGroup).GetMember(group.ToString())[0].GetCustomAttributes(typeof(System.ObsoleteAttribute), false).Length != 0)
+					continue;
+
+				// Group is nothing.
 				if ((int)group <= 0)
 					continue;
+
 				try
 				{
+					Debug.Log(group + ", " + (int)group);
 					PlayerSettings.SetScriptingDefineSymbolsForGroup(group, "");
 					PlayerSettings.SetScriptingDefineSymbolsForGroup(group, defineSymbols);
 				}
@@ -52,66 +68,90 @@ namespace Mobcast.Coffee.Editor
 					Debug.LogException(e);
 				}
 			}
-
 		}
 
-		/// <summary>定義シンボルリストを戻します.</summary>
+		/// <summary>
+		/// 定義シンボルリストを戻します.
+		/// Revert 'Scripting Define Symbols' to project settings.
+		/// </summary>
 		public void Revert()
 		{
 			string define = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
 		
-			IEnumerable<string> currentDefines = define.Replace(" ", "").Split(new char[] { ';' });
+			// 現在の有効シンボル.
+			// Current active symbols.
+			IEnumerable<string> currentDefines = define.Replace(" ", "")
+				.Split(new char[] { ';' })
+				.Where(x => !string.IsNullOrEmpty(x));
 
-			//カタログに登録済みのシンボルを判定.
-			list.ForEach(x =>
-				{
-					x.enabled = !string.IsNullOrEmpty(x.name) && currentDefines.Contains(x.name);
-					if (x.enabled && x.style != SymbolStyle.Symbol)
-					{
-						foreach (var style in list.Where(style=>style.style == x.style))
-							style.enabled = false;
-						x.enabled = true;
-					}
+			// カタログに登録済みのシンボルを判定.
+			// Is sybmol enable?
+			list.ForEach(symbol => symbol.enabled = currentDefines.Contains(symbol.name));
 
-				});
-
-			//カタログに登録されていないものもシンボルとして追加.
-			foreach (string name in currentDefines.Where(x => !string.IsNullOrEmpty(x) && !list.Any(y => y.name == x)))
+			// 登録されていないシンボルをカタログに追加.
+			// Add symbols that are not added in the catalog.
+			foreach (string symbolName in currentDefines.Where(x => list.All(y => y.name != x)))
 			{
-				list.Add(new Symbol() { enabled = true, name = name });
+				list.Add(new Symbol() { enabled = true, name = symbolName });
 			}
 		}
 
 		/// <summary>
 		/// シンボルスタイル.
+		/// Style.
 		/// </summary>
 		public enum SymbolStyle
 		{
-			/// <summary>通常シンボル.</summary>
+			/// <summary>
+			/// 通常シンボル.
+			/// Default symbol.
+			/// </summary>
 			Symbol = 1,
-			/// <summary>セパレータ.</summary>
+
+			/// <summary>
+			/// セパレータ.
+			/// Separator.
+			/// </summary>
 			Separator = 10,
-			/// <summary>ヘッダー.</summary>
+
+			/// <summary>
+			/// ヘッダー.
+			/// Header.
+			/// </summary>
 			Header,
 		}
 
 		/// <summary>
 		/// 定義シンボルクラス.
-		/// シンボル名と説明、グループを管理します.
+		/// シンボル名と説明、スタイルを管理します.
+		/// Symbol.
+		/// Included header and separator.
 		/// </summary>
 		[System.Serializable]
 		public class Symbol
 		{
-			/// <summary>シンボルスタイル.</summary>
+			/// <summary>
+			/// シンボルスタイル.
+			/// Style.
+			/// </summary>
 			public SymbolStyle style = SymbolStyle.Symbol;
 
-			/// <summary>定義シンボルは有効か.</summary>
+			/// <summary>
+			/// 定義シンボルは有効か.
+			/// Is sybmol enable?
+			/// </summary>
 			public bool enabled { get; set; }
 
-			/// <summary>定義シンボル名.</summary>
+			/// <summary>
+			/// 定義シンボル名.
+			/// Name.
+			/// </summary>
 			public string name = "";
 
-			/// <summary>定義シンボル説明.</summary>
+			/// <summary>
+			/// 定義シンボル説明.
+			/// Description.
+			/// </summary>
 			public string description = "";
 		}
 	}
@@ -123,18 +163,18 @@ namespace Mobcast.Coffee.Editor
 	{
 		SymbolCatalog catalog;
 		string currentDefine;
+		string focus;
 		Vector2 scrollPosition;
 
 		//---- ▼ GUIキャッシュ ▼ ----
 		static readonly ReorderableList ro = new ReorderableList(new List<SymbolCatalog>(), typeof(SymbolCatalog));
-		static GUIContent contentApply;
 		static GUIStyle styleTitle;
 		static GUIStyle styleHeader;
 		static GUIStyle styleDescription;
 		static readonly Color EnableStyleColor = new Color(0.5f, 0.5f, 0.5f, 1f);
 		static readonly Color EnableTextColor = Color.white;
 		static readonly Color DisableStyleColor = Color.white;
-		static readonly Color DisableTextColor = new Color(1, 1, 1, 0.5f);
+		static readonly Color DisableTextColor = new Color(1, 1, 1, 0.8f);
 
 		void Initialize()
 		{
@@ -157,18 +197,15 @@ namespace Mobcast.Coffee.Editor
 			// ヘッダースタイル
 			styleHeader = new GUIStyle("VCS_StickyNote");
 			styleHeader.richText = true;
-			styleHeader.fontSize = 13;
+			styleHeader.fontSize = 12;
 			styleHeader.fontStyle = FontStyle.Bold;
 			styleHeader.padding = new RectOffset(25, 3, 2, 2);
 			styleHeader.alignment = TextAnchor.MiddleLeft;
 			styleHeader.wordWrap = false;
 
-			contentApply = new GUIContent("Apply", EditorGUIUtility.FindTexture("vcs_check"));
-
 			// シンボルリスト
 			ro.drawElementCallback = DrawSymbol;
 			ro.headerHeight = 0;
-//			ro.drawHeaderCallback = rect => GUI.Label(rect, "Available Script Symbols");
 			ro.onAddDropdownCallback = (rect, list) =>
 			{
 				var gm = new GenericMenu();
@@ -177,12 +214,7 @@ namespace Mobcast.Coffee.Editor
 				gm.AddItem(new GUIContent("Separator"), false, () => AddSymbol(SymbolCatalog.SymbolStyle.Separator));
 				gm.DropDown(rect);
 			};
-			ro.onRemoveCallback = list =>
-			{
-				catalog.list.RemoveAt(list.index);
-				if (0 < list.index)
-					list.index--;
-			};
+			ro.onRemoveCallback = list => RemoveSymbol(catalog.list[list.index]);
 			ro.onCanRemoveCallback = list => (0 <= list.index && list.index < catalog.list.Count);
 			ro.elementHeight = 44;
 			ro.onSelectCallback = (list) => EditorGUIUtility.keyboardControl = 0;
@@ -191,15 +223,16 @@ namespace Mobcast.Coffee.Editor
 		}
 		//---- ▲ GUIキャッシュ ▲ ----
 
-		int addIndex = -1;
-
 		[MenuItem("Coffee/Symbol Catalog")]
 		static void OnOpenFromMenu()
 		{
 			EditorWindow.GetWindow<SymbolCatalogEditor>("Symbol Catalog");
 		}
 
-		/// <summary>新しいScript Define Symbolsアセットを生成します.</summary>
+		/// <summary>
+		/// 新しいSymbolCatalogアセットを生成します.
+		/// Create new SymbolCatalog asset.
+		/// </summary>
 		static SymbolCatalog CreateCatalog()
 		{
 			if (!Directory.Exists("Assets/Editor"))
@@ -214,20 +247,23 @@ namespace Mobcast.Coffee.Editor
 
 
 		/// <summary>
-		/// インスペクタを表示.
+		/// GUIを表示します.
+		/// Draw GUI.
 		/// </summary>
 		void OnGUI()
 		{
 			Initialize();
 
-			// カタログアセットを取得/作成
+			// カタログアセットを取得/作成します.
+			// Get/Create catalog asset.
 			catalog = catalog
 			?? AssetDatabase.FindAssets("t:" + typeof(SymbolCatalog).Name)
-				.Select(x => AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(x), typeof(SymbolCatalog)) as SymbolCatalog)
-				.FirstOrDefault()
+						.Select(x => AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(x), typeof(SymbolCatalog)) as SymbolCatalog)
+						.FirstOrDefault()
 			?? CreateCatalog();
 
-			//変更チェック.PlayerSettingsなどで直接変えた場合も検知.
+			// 変更チェック.PlayerSettingsなどで直接変えた場合も検知します.
+			// Sync script define symbol.
 			string define = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
 			if (currentDefine != define)
 			{
@@ -238,135 +274,195 @@ namespace Mobcast.Coffee.Editor
 			using (var svs = new EditorGUILayout.ScrollViewScope(scrollPosition))
 			{
 				scrollPosition = svs.scrollPosition;
+				EditorGUI.BeginChangeCheck();
 
-				//
-				GUILayout.Label(new GUIContent("   Available Script Symbol Define", EditorGUIUtility.ObjectContent(catalog, typeof(SymbolCatalog)).image), styleTitle);
+				// タイトル
+				// Title
+				GUILayout.Label(new GUIContent("   Available Scripting Define Symbols", EditorGUIUtility.ObjectContent(catalog, typeof(SymbolCatalog)).image), styleTitle);
 
-				//シンボルリスト表示.
+				// シンボルリスト.
+				// Draw all symbols in catalog.
 				ro.list = catalog.list;
 				ro.DoLayoutList();
 
-
-				using (new EditorGUILayout.HorizontalScope())
+				// Applyボタン.
+				// Button to apply scripting define symbols.
+				using (new EditorGUI.DisabledGroupScope(EditorApplication.isCompiling))
 				{
-					GUILayout.FlexibleSpace();
-
-					//Applyボタン. PlayerSettingsのDefineSymbolに適用.
-					using (new EditorGUI.DisabledGroupScope(EditorApplication.isCompiling))
+					using (new EditorGUILayout.HorizontalScope(EditorStyles.helpBox))
 					{
-						if (GUILayout.Button(contentApply, GUILayout.Width(57)))
+						GUILayout.Label(new GUIContent("Apply To", EditorGUIUtility.FindTexture("vcs_check")));
+
+						if (GUILayout.Button("All Targets"))
 						{
 							catalog.Apply();
 						}
+						if (GUILayout.Button(string.Format("Current ({0})", EditorUserBuildSettings.selectedBuildTargetGroup)))
+						{
+							catalog.Apply(EditorUserBuildSettings.selectedBuildTargetGroup);
+						}
 					}
 				}
+
+				if (EditorGUI.EndChangeCheck())
+					EditorUtility.SetDirty(catalog);
 			}
 
-			//コンパイル中.
+			// コンパイル中.
+			// Request repain during compiling.
 			if (EditorApplication.isCompiling)
 			{
 				Repaint();
 			}
+
+			// Addされたら自動でシンボル名の入力開始します.
+			// When add new symbol, focus input field.
+			if (!string.IsNullOrEmpty(focus))
+			{
+				EditorGUI.FocusTextInControl(focus);
+				focus = null;
+			}
 		}
 
 		/// <summary>
-		/// シンボルを表示.
+		/// シンボルをカタログに追加します.
+		/// Add symbol to catalog.
 		/// </summary>
 		void AddSymbol(SymbolCatalog.SymbolStyle style)
 		{
-			catalog.list.Add(new SymbolCatalog.Symbol(){
-				style = style,
-				name = style == SymbolCatalog.SymbolStyle.Symbol ? "NEW_SYMBOL" : "",
-				description = style != SymbolCatalog.SymbolStyle.Separator ? "Comment(<i>option, ritch-text is available</i>)" : "",
-			});
-			addIndex = catalog.list.Count - 1;
+			// 新しいシンボルを作成します.
+			// Create new symbol.
+			SymbolCatalog.Symbol symbol = new SymbolCatalog.Symbol(){ style = style };
+			switch (style)
+			{
+				case SymbolCatalog.SymbolStyle.Symbol:
+					symbol.name = "SYMBOL_NAME";
+					symbol.description = "symbol description(<i>ritch-text is available</i>)";
+					break;
+				case SymbolCatalog.SymbolStyle.Header:
+					symbol.name = "Header(<i>ritch-text is available</i>)";
+					break;
+				case SymbolCatalog.SymbolStyle.Separator:
+					break;
+			}
+
+			// シンボルをカタログに追加します.
+			// Add symbol to catalog.
+			catalog.list.Add(symbol);
+
+			// シンボル名の編集にフォーカス.
+			// Focus to input field.
+			focus = string.Format("symbol neme {0}", catalog.list.IndexOf(symbol));
+
 			EditorUtility.SetDirty(catalog);
 		}
 
 		/// <summary>
-		/// シンボルを表示.
+		/// シンボルをカタログに追加します.
+		/// Add symbol to catalog.
+		/// </summary>
+		void RemoveSymbol(SymbolCatalog.Symbol symbol)
+		{
+			EditorApplication.delayCall += () =>
+			{
+				catalog.list.Remove(symbol);
+				ro.index = Mathf.Clamp(ro.index, 0, catalog.list.Count - 1);
+				EditorUtility.SetDirty(catalog);
+				Repaint();
+			};
+		}
+
+		/// <summary>
+		/// シンボルを描画します.
+		/// Draw symbol.
 		/// </summary>
 		void DrawSymbol(Rect rect, int index, bool isActive, bool isFocused)
 		{
-			SymbolCatalog.Symbol currentSymbol = ro.list[index] as SymbolCatalog.Symbol;
+			SymbolCatalog.Symbol symbol = ro.list[index] as SymbolCatalog.Symbol;
 
-			//セパレータの場合、線引くだけ.
-			if (currentSymbol.style == SymbolCatalog.SymbolStyle.Separator)
+			// シンボルスタイルに応じて描画します.
+			// Draw symbol based on style.
+			switch (symbol.style)
 			{
-				GUI.Label(new Rect(rect.x + 10, rect.y + 24, rect.width - 20, 16), GUIContent.none, "sv_iconselector_sep");
+				case SymbolCatalog.SymbolStyle.Symbol:
+					DrawDefaultSymbol(rect, symbol);
+					break;
+				case SymbolCatalog.SymbolStyle.Separator:
+					GUI.Label(new Rect(rect.x + 10, rect.y + 24, rect.width - 20, 16), GUIContent.none, "sv_iconselector_sep");
+					break;
+				case SymbolCatalog.SymbolStyle.Header:
+					DrawHeader(rect, symbol);
+					break;
 			}
-			//ヘッダーの場合、説明テキストだけ.
-			else if (currentSymbol.style == SymbolCatalog.SymbolStyle.Header)
-			{
-				GUI.contentColor = Color.black;
-				string symbolDescriptionId = string.Format("symbol desctription {0}", index);
-				GUI.SetNextControlName(symbolDescriptionId);
-				styleHeader.richText = GUI.GetNameOfFocusedControl() != symbolDescriptionId;
-				currentSymbol.description = GUI.TextField(new Rect(rect.x - 19, rect.y + rect.height - 24, rect.width + 23, 20), currentSymbol.description, styleHeader);
-				GUI.contentColor = Color.white;
-			}
-			//通常シンボル.
-			else
-			{
-				DrawDefaultSymbol(rect, index, currentSymbol);
-			}
-
-			EditorUtility.SetDirty(catalog);
+			
 			GUI.color = Color.white;
 			GUI.contentColor = Color.white;
 		}
 
+
 		/// <summary>
-		/// 通常シンボルを表示.
+		/// ヘッダーを描画します.
+		/// Draw header.
 		/// </summary>
 		/// <param name="rect">描画座標.</param>
 		/// <param name="index">シンボルインデックス.</param>
 		/// <param name="symbol">表示するシンボル.</param>
-		void DrawDefaultSymbol(Rect rect, int index, SymbolCatalog.Symbol symbol)
+		void DrawHeader(Rect rect, SymbolCatalog.Symbol symbol)
 		{
-			//シンボル説明を描画します.
+			int index = catalog.list.IndexOf(symbol);
+
+			// シンボル名を描画します.
+			// Draw symbol name(editable).
+			GUI.contentColor = Color.black;
+			string symbolNameId = string.Format("symbol neme {0}", index);
+			GUI.SetNextControlName(symbolNameId);
+			styleHeader.richText = GUI.GetNameOfFocusedControl() != symbolNameId;
+			symbol.name = GUI.TextField(new Rect(rect.x - 19, rect.y + rect.height - 24, rect.width + 23, 20), symbol.name, styleHeader);
+			GUI.contentColor = Color.white;
+		}
+
+		/// <summary>
+		/// 通常シンボルを描画します.
+		/// Draw default symbol.
+		/// </summary>
+		/// <param name="rect">描画座標.</param>
+		/// <param name="index">シンボルインデックス.</param>
+		/// <param name="symbol">表示するシンボル.</param>
+		void DrawDefaultSymbol(Rect rect, SymbolCatalog.Symbol symbol)
+		{
+			int index = catalog.list.IndexOf(symbol);
+
+			// シンボル説明を描画します.
+			// Draw symbol description(editable).
 			string symbolDescriptionId = string.Format("symbol desctription {0}", index);
 			GUI.SetNextControlName(symbolDescriptionId);
 			styleDescription.richText = GUI.GetNameOfFocusedControl() != symbolDescriptionId;
 			symbol.description = GUI.TextArea(new Rect(rect.x, rect.y + 12, rect.width, rect.height - 13), symbol.description, styleDescription);
 
-			//背景を描画します.
+			// 背景を描画します.
+			// Draw symbol name background.
 			GUI.color = symbol.enabled ? EnableStyleColor : DisableStyleColor;
 			GUI.Label(new Rect(rect.x, rect.y, rect.width, 16), GUIContent.none, "ShurikenEffectBg");//"flow node flow" + (int)symbol.style);
 			GUI.color = Color.white;
 
-			//有効トグルを描画します.
-			//				bool isGrouped = symbol.style != SymbolCatalog.SymbolStyle.Symbol;
-			bool isGrouped = false;
-			bool isEnable = EditorGUI.Toggle(new Rect(rect.x + 5, rect.y, 15, 16), symbol.enabled, isGrouped ? "Radio" : "Toggle");
-			if (symbol.enabled != isEnable)
-			{
-				//グループシンボルの場合、同じグループのシンボルを無効化.
-				if (isGrouped)
-				{
-					foreach (var style in catalog.list.Where(style=>style.style == symbol.style))
-						style.enabled = false;
-					symbol.enabled = true;
-				}
-				else
-				{
-					symbol.enabled = isEnable;
-				}
-			}
+			// トグルを描画します.
+			// Draw toggle.
+			symbol.enabled = GUI.Toggle(new Rect(rect.x + 5, rect.y, 15, 16), symbol.enabled, GUIContent.none);
 
-			//シンボル名を描画します.
+			// シンボル名を描画します.
+			// Draw symbol name(editable).
 			string symbolNameId = string.Format("symbol neme {0}", index);
 			GUI.SetNextControlName(symbolNameId);
 			GUI.color = symbol.enabled ? EnableTextColor : DisableTextColor;
 			GUIStyle labelStyle = EditorGUIUtility.isProSkin ? EditorStyles.boldLabel : EditorStyles.whiteBoldLabel;
-			symbol.name = GUI.TextField(new Rect(rect.x + 21, rect.y, rect.width - 50, 16), symbol.name, labelStyle);
+			symbol.name = GUI.TextField(new Rect(rect.x + 20, rect.y, rect.width - 40, 16), symbol.name, labelStyle);
 			GUI.color = Color.white;
 
-			//Addされたら自動でシンボル名の入力開始します.
-			if (addIndex == index)
+			// シンボル削除ボタンを描画します.
+			// Draw delete button.
+			if (GUI.Button(new Rect(rect.x + rect.width - 20, rect.y, 20, 20), EditorGUIUtility.FindTexture("treeeditor.trash"), EditorStyles.label))
 			{
-				EditorGUI.FocusTextInControl(symbolNameId);
-				addIndex = -1;
+				RemoveSymbol(symbol);
 			}
 		}
 	}
